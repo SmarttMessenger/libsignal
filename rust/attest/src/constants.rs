fa5d51fbfce462cad290c05e23ce7d16efdac469
Eloy <eloy@smarttmessenger.com>
2026-05-27 21:42:43 +0200

    Servers, certificates & enclaves customization

--- rust/attest/src/constants.rs
+++ rust/attest/src/constants.rs
@@ -64,7 +64,7 @@ def_enclaves! {
     ENCLAVE_ID_SVRB_STAGING => ("a75542d82da9f6914a1e31f8a7407053b99cc99a0e7291d8fbd394253e19b036", common),
     ENCLAVE_ID_SVRB_PROD => ("aa906dbc85965d37accb660b65a8c224f037b0e7cfd034532acada3592e5b446", common),
     ENCLAVE_ID_CDSI_STAGING => ("3ded708ca5a42fd84b4639dc661a7ec4b9c9f1b92809c0fc91da2349a5a89d05", common),
-    ENCLAVE_ID_CDSI_PROD => ("ee9503070127120074612b6688e593b67e486b1541449f54d71e387484eb40a3", common),
+    ENCLAVE_ID_CDSI_PROD => ("3ded708ca5a42fd84b4639dc661a7ec4b9c9f1b92809c0fc91da2349a5a89d05", common),
 }
 
 /// SW advisories known to be mitigated by default. If an MREnclave is provided that
--- rust/net/src/env.rs
+++ rust/net/src/env.rs
@@ -39,21 +39,18 @@ pub(crate) const CONNECTED_ELSEWHERE_CLOSE_CODE: u16 = 4409;
 
 const DOMAIN_CONFIG_CHAT: DomainConfig = DomainConfig {
     ip_v4: &[
-        ip_addr!(v4, "76.223.92.165"),
-        ip_addr!(v4, "13.248.212.111"),
+        ip_addr!(v4, "44.227.47.178"),
     ],
     ip_v6: &[
-        ip_addr!(v6, "2600:9000:a507:ab6d:4ce3:2f58:25d7:9cbf"),
-        ip_addr!(v6, "2600:9000:a61f:527c:d5eb:a431:5239:3232"),
     ],
     connect: ConnectionConfig {
-        hostname: "chat.signal.org",
+        hostname: "chat.staging.smarttmessenger.com",
         port: DEFAULT_HTTPS_PORT,
         cert: SIGNAL_ROOT_CERTIFICATES,
         min_tls_version: Some(SslVersion::TLS1_3),
         confirmation_header_name: Some(TIMESTAMP_HEADER_NAME),
         proxy: Some(ConnectionProxyConfig {
-            path_prefix: "/service",
+            path_prefix: "/service-staging",
             configs: [PROXY_CONFIG_F_PROD, PROXY_CONFIG_G],
         }),
     },
@@ -61,15 +58,12 @@ const DOMAIN_CONFIG_CHAT: DomainConfig = DomainConfig {
 
 const DOMAIN_CONFIG_CHAT_STAGING: DomainConfig = DomainConfig {
     ip_v4: &[
-        ip_addr!(v4, "76.223.72.142"),
-        ip_addr!(v4, "13.248.206.115"),
+        ip_addr!(v4, "44.227.47.178"),
     ],
     ip_v6: &[
-        ip_addr!(v6, "2600:9000:a507:ab6d:7b25:2580:8bd6:3b93"),
-        ip_addr!(v6, "2600:9000:a61f:527c:2215:cd9:bac6:a2f8"),
     ],
     connect: ConnectionConfig {
-        hostname: "chat.staging.signal.org",
+        hostname: "chat.staging.smarttmessenger.com",
         port: DEFAULT_HTTPS_PORT,
         cert: SIGNAL_ROOT_CERTIFICATES,
         min_tls_version: Some(SslVersion::TLS1_3),
@@ -83,23 +77,23 @@ const DOMAIN_CONFIG_CHAT_STAGING: DomainConfig = DomainConfig {
 
 const DOMAIN_CONFIG_CDSI: DomainConfig = DomainConfig {
     connect: ConnectionConfig {
-        hostname: "cdsi.signal.org",
+        hostname: "cdsi.staging.smarttmessenger.com",
         port: DEFAULT_HTTPS_PORT,
         cert: SIGNAL_ROOT_CERTIFICATES,
         min_tls_version: Some(SslVersion::TLS1_3),
         confirmation_header_name: None,
         proxy: Some(ConnectionProxyConfig {
-            path_prefix: "/cdsi",
+            path_prefix: "/cdsi-staging",
             configs: [PROXY_CONFIG_F_PROD, PROXY_CONFIG_G],
         }),
     },
-    ip_v4: &[ip_addr!(v4, "40.122.45.194")],
-    ip_v6: &[ip_addr!(v6, "2603:1030:7::1")],
+    ip_v4: &[ip_addr!(v4, "13.91.71.83")],
+    ip_v6: &[],
 };
 
 const DOMAIN_CONFIG_CDSI_STAGING: DomainConfig = DomainConfig {
     connect: ConnectionConfig {
-        hostname: "cdsi.staging.signal.org",
+        hostname: "cdsi.staging.smarttmessenger.com",
         port: DEFAULT_HTTPS_PORT,
         cert: SIGNAL_ROOT_CERTIFICATES,
         min_tls_version: Some(SslVersion::TLS1_3),
@@ -109,8 +103,8 @@ const DOMAIN_CONFIG_CDSI_STAGING: DomainConfig = DomainConfig {
             configs: [PROXY_CONFIG_F_STAGING, PROXY_CONFIG_G],
         }),
     },
-    ip_v4: &[ip_addr!(v4, "104.43.162.137")],
-    ip_v6: &[ip_addr!(v6, "2603:1030:7::732")],
+    ip_v4: &[ip_addr!(v4, "13.91.71.83")],
+    ip_v6: &[],
 };
 
 const DOMAIN_CONFIG_SVR2: DomainConfig = DomainConfig {
